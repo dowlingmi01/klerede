cfb7cbb406cbb9ceea8055bc78225cfff0782f40
sdaicz <sdaicz@klerede.com>
2015-04-22 13:42:06 -0300

    Add model for store product category and import it from product description. re #31

--- laravel/app/StoreProduct.php
+++ laravel/app/StoreProduct.php
@@ -8,6 +8,11 @@ class StoreProduct extends Model {
 	static function getForXML(\SimpleXMLElement $xmlProduct) {
 		$store_product = StoreProduct::firstOrNew(['code'=>$xmlProduct->ItemID]);
 		$store_product->description = $xmlProduct->Description;
+		if($xmlProduct['ItemType']=='dtv:NonMerchandise')
+			$category_code = 'NME';
+		else
+			$category_code = substr($store_product->description, 0, 3);
+		$store_product->store_product_category_id = StoreProductCategory::getFor($category_code)->id;
 		$store_product->scanned_code = $xmlProduct->children('dtv', true)->ScannedItemID;
 		$store_product->save();
 		return $store_product;
---
+++ laravel/app/StoreProductCategory.php
@@ -0,0 +1,12 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class StoreProductCategory extends Model {
+	protected $table = 'store_product_category';
+	protected $guarded = [];
+	static function getFor($code) {
+		return StoreProductCategory::firstOrCreate(['code'=>$code]);
+	}
+
+}
