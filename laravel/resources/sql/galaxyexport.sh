26655db4e7519928b5c62a6cf6c8422d0a90e368
sdaicz <sdaicz@klerede.com>
2015-06-09 19:54:22 -0300

    Script for exporting Galaxy data. re #39

---
+++ laravel/resources/sql/galaxyexport.sh
@@ -0,0 +1,4 @@
+bcp 'SELECT * FROM export..transaction_header ORDER BY source_id' queryout tranheader.txt -cT -d export
+bcp 'SELECT * FROM export..transaction_line ORDER BY source_id, sequence' queryout tranline.txt -cT -d export
+bcp 'SELECT * FROM export..item ORDER BY code' queryout item.txt -cT -d export
+bcp 'SELECT * FROM export..visit ORDER BY source_id' queryout visit.txt -cT -d export
