fa033b8b9eba775613855f12cdb5758f14402557
Taylor Johnson <webninjataylor@gmail.com>
2015-07-07 23:21:24 -0400

    More basic layout and added temp files to .gitignore.

--- laravel/resources/views/layout.blade.php
+++ laravel/resources/views/layout.blade.php
@@ -5,12 +5,27 @@
         <link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
     </head>
     <body>
-        <header>
-            <img src="img/logo-klerede.png" class="logo-main" />
-            <img src="img/logo-livingplanet.png" class="logo-client" />
+        <header class="container-fluid">
+            <div class="row">
+                <div class="col-xs-6 col-md-6"><img src="img/logo-klerede.png" class="logo-main" /></div>
+                <div class="col-xs-6 col-md-6"><img src="img/logo-livingplanet.png" class="logo-client" /></div>
+            </div>
         </header>
+        <section class="container-fluid">
+            <div class="row">
+                <div class="col-xs-2 col-md-2 menu">
+                    <ul>
+                        <li>Dashboard</li>
+                        <li>Tool Box</li>
+                        <li>Members</li>
+                        <li>Campaigns</li>
+                    </ul>
+                </div>
+                <div class="col-xs-10 col-md-10">@yield('content')</div>
+            </div>
+        </section>
 
-        @yield('content')
+        
 
         <script src="js/libs.js"></script>
         <script src="js/app.js"></script>
