0d82388ed0a0dcd86edfd9e6d26719112bf0a06a
Taylor Johnson <webninjataylor@gmail.com>
2015-07-08 21:15:15 -0400

    Fixed CSS override issue and streamlined Gulp.

--- laravel/gulpfile.js
+++ laravel/gulpfile.js
@@ -22,33 +22,15 @@ elixir.config.sourcemaps = false;
 /******** TASKS ********/
 /***********************/
 
-// GULP defaults ... 'gulp watch' will compile the scss, minify, AND move it to the public folder ... but gulp clean must be run after development to remove the temp files
+// GULP defaults ... 'gulp watch' will compile the scss, minify, AND move it to the public folder
 elixir(function(mix) {
     // Sergio's report task
     mix.browserify('report.js', elixir.jsOutput, 'resources/js', {debug:true});
-    // MASTER STYLES PROCESSING ... 1) Compile LESS, 2) compile SASS, 3) combine (order of concatenation, destination, source), 4) remove temp files
-    mix.less('app.less','public/css/temp-less.css')
-        .sass('*','public/css/temp-sass.css')
-        .styles(
-            ['temp-sass.css'],
-            'public/css/app.css',
-            'public/css'
-        )
-        .task('clean');
-    // MASTER SCRIPTS PROCESSING ... 1) Set scripts in order of concatenation, 2) declare destination, 3) set source path for scripts
-    mix.scripts(
-        ['utils.js', 'dashboard.js'],
-        'public/js/app.js',
-        'resources/js/app'
-    );
-});
-
-// Remove temp files
-gulp.task('clean', function () {
-  del([
-    'public/css/temp-less.css',
-    'public/css/temp-sass.css'
-  ]);
+    // Compile LESS, compile SASS, compile JS libraries, and compile custom JS
+    mix.less('app.less','public/css/app.css')
+        .sass('dashboard.scss','public/css/dashboard.css')
+        .task('libs')
+        .task('app');
 });
 
 // Lint JavaScript
