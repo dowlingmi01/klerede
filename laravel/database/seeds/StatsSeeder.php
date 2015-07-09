51f3d93bd99d782772776cffeaf848de695bbcf7
sdaicz <sdaicz@klerede.com>
2015-07-10 00:52:52 +0200

    Add seed data for box office sales and visits statistics. re #136

---
+++ laravel/database/seeds/StatsSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class StatsSeeder extends Seeder {
+
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run() {
+		$sql = gzdecode(file_get_contents(database_path('seeds/data/stats.sql.gz')));
+		DB::unprepared($sql);
+	}
+}
