4f038448770ec232f8d75bad2594cdca230fe19b
Taylor Johnson <webninjataylor@gmail.com>
2015-07-22 16:20:36 -0400

    Welcome message clock and date functional.

--- laravel/resources/js/app/utils.js
+++ laravel/resources/js/app/utils.js
@@ -1,2 +1,43 @@
 /******** HELPER UTILITIES ********/
+var wnt ={
+    months: [
+        'January', 
+        'February', 
+        'March', 
+        'April', 
+        'May', 
+        'June', 
+        'July', 
+        'August', 
+        'September', 
+        'October', 
+        'November', 
+        'December'
+    ],
+    updateClock: function(){
+        var today = new Date(),
+            hours = today.getHours(),
+            minutes = today.getMinutes(),
+            date = today.getDate(),
+            month = wnt.months[today.getMonth()],
+            year = today.getFullYear(),
+            time,
+            period;
+        // Handle zeros in front of minutes
+        minutes = minutes < 10 ? '0'+minutes : minutes;
+        if(hours === 0){
+            time = '12:'+minutes;
+            period = 'AM';
+        }
+        else if(hours < 12){
+            time = hours+':'+minutes;
+            period = 'AM';
+        }
+        else {
+            time = (hours-12)+':'+minutes;
+            period = 'PM';
+        }
+        return [time, period, date, month+' '+year]
+    }
+};
 console.log('Utilities loaded...');
--- laravel/resources/js/app/welcome-text.js
+++ laravel/resources/js/app/welcome-text.js
@@ -1,22 +1,17 @@
 /* Welcome Text */
 var WelcomeText = React.createClass({
     getInitialState: function() {
-        this.todaysDate;
         return {
             firstName: 'Joe',
-            currentDate: 1
+            clock: wnt.updateClock()
         };
     },
-    todaysDate: function(){
-        var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-        var d = new Date();
-        var y = d.getFullYear();
-        var m = months[d.getMonth()];
-        var lesDate = d.getDate();
-        var hr = d.getHours();
-        var min = d.getMinutes();
-        this.setState({ currentDate: lesDate });
-        console.log(lesDate);
+    updateClock: function(){
+        this.setState({ clock: wnt.updateClock() });
+        setTimeout(this.updateClock, 1000);  //change to 60000?
+    },
+    componentDidMount: function(){
+        this.updateClock();
     },
     render: function() {
         return (
@@ -25,8 +20,8 @@ var WelcomeText = React.createClass({
                 <span className='user-name'>{this.state.firstName}!</span>&#160;
                 Things are looking good.
                 <div className='time-date'>
-                    12:00 <span className='time-date-modifier'>PM</span>&#160;&#160;&#160;
-                    {this.state.currentDate} <span className='time-date-modifier'>July 2015</span>
+                    {this.state.clock[0]} <span className="time-period">{this.state.clock[1]}</span>
+                    {this.state.clock[2]} <span className="month-year">{this.state.clock[3]}</span>
                 </div>
             </div>
         );
