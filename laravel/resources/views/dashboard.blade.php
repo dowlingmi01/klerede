94a7b5ffd86bf5472465508c3aae2e49a0fb0ceb
Taylor Johnson <webninjataylor@gmail.com>
2015-07-27 21:36:21 -0400

    Fixed bottom blocks to flow like the top blocks.

--- laravel/resources/views/dashboard.blade.php
+++ laravel/resources/views/dashboard.blade.php
@@ -81,42 +81,42 @@
         </div>
     </div>
     <div class="row row-eq-height">
-        <div class="col-xs-2 col-md-2">
+        <div class="col-xs-6 col-sm-4 col-lg-2">
             <div class="stat-block stats-bottom member-conversion">
                 <div class="label">Member Conversion</div>
                 <div class="stat">4.0%</div>
                 <div class="change up"><span class="glyphicon glyphicon-arrow-up" aria-hidden="true"></span> 4.8%</div>
             </div>
         </div>
-        <div class="col-xs-2 col-md-2">
+        <div class="col-xs-6 col-sm-4 col-lg-2">
             <div class="stat-block stats-bottom average">
                 <div class="label">Average</div>
                 <div class="stat">4.1%</div>
                 <div class="change up"><span class="glyphicon glyphicon-arrow-up" aria-hidden="true"></span> 3.8%</div>
             </div>
         </div>
-        <div class="col-xs-2 col-md-2">
+        <div class="col-xs-6 col-sm-4 col-lg-2">
             <div class="stat-block stats-bottom frequency">
                 <div class="label">Frequency</div>
                 <div class="stat">3.6</div>
                 <div class="change up"><span class="glyphicon glyphicon-arrow-up" aria-hidden="true"></span> 3.2</div>
             </div>
         </div>
-        <div class="col-xs-2 col-md-2">
+        <div class="col-xs-6 col-sm-4 col-lg-2">
             <div class="stat-block stats-bottom regency">
                 <div class="label">Regency</div>
                 <div class="stat">4.1</div>
                 <div class="change up"><span class="glyphicon glyphicon-arrow-up" aria-hidden="true"></span> 4.8</div>
             </div>
         </div>
-        <div class="col-xs-2 col-md-2">
+        <div class="col-xs-6 col-sm-4 col-lg-2">
             <div class="stat-block stats-bottom members">
                 <div class="label">Members</div>
                 <div class="stat">4.2k</div>
                 <div class="change up"><span class="glyphicon glyphicon-arrow-up" aria-hidden="true"></span> 3.8k</div>
             </div>
         </div>
-        <div class="col-xs-2 col-md-2">
+        <div class="col-xs-6 col-sm-4 col-lg-2">
             <div class="stat-block stats-bottom renewal-velocity">
                 <div class="label">Renewal Velocity</div>
                 <div class="stat">5.1</div>
