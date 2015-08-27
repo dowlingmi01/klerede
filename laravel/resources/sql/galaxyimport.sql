c2cca07fc7a56653e87c179818e669777a3678fd
sdaicz <sdaicz@klerede.com>
2015-08-26 21:56:24 -0300

    Import membership data from Galaxy data. re #39

--- laravel/resources/sql/galaxyimport.sql
+++ laravel/resources/sql/galaxyimport.sql
@@ -42,3 +42,44 @@ SELECT source_id, v.venue_id, acp_id, p.id, ticket_code, v.kind, operation_id, q
  WHERE v.box_office_product_code = p.code
    AND v.venue_id = p.venue_id
 ;
+
+LOAD DATA INFILE 'galaxyimport/membership.txt'
+INTO TABLE membership_galaxy
+FIELDS ESCAPED BY ''
+LINES TERMINATED BY '\r\n'
+(venue_id, member_code, code, sequence, box_office_product_code, date_from, date_to, @dob, adult_qty, child_qty
+, first, middle, last, street_1, street_2, city, state, country, phone)
+SET dob = if(@dob > '1900-01-02', @dob, NULL)
+;
+
+INSERT member_address
+     ( street_1, street_2, city, state, country, phone )
+SELECT DISTINCT street_1, street_2, city, state, country, phone
+  FROM membership_galaxy
+;
+INSERT member_name
+     ( first, middle, last )
+SELECT DISTINCT first, middle, last
+  FROM membership_galaxy
+;
+INSERT member
+     ( venue_id, code, last_membership_id)
+SELECT DISTINCT venue_id, member_code, 0
+  FROM membership_galaxy
+;
+INSERT membership
+     ( venue_id, member_id, member_address_id, member_name_id, code, sequence, box_office_product_id
+     , date_from, date_to, dob, adult_qty, child_qty )
+SELECT g.venue_id, m.id, a.id, n.id, g.code, g.sequence, p.id
+     , date_from, date_to, dob, adult_qty, child_qty
+  FROM membership_galaxy g
+  JOIN member m ON g.venue_id = m.venue_id AND g.member_code = m.code
+  JOIN box_office_product p ON g.venue_id = p.venue_id AND g.box_office_product_code = p.code
+  JOIN member_address a ON g.street_1 = a.street_1 AND g.street_2 = a.street_2 AND g.city = a.city
+       AND g.state = a.state AND g.country = a.country AND g.phone = a.phone
+  JOIN member_name n ON g.first = n.first AND g.middle = n.middle AND g.last = n.last
+;
+UPDATE member, (SELECT member_id, max(id) as maxid FROM membership GROUP BY member_id) x
+   SET member.last_membership_id = x.maxid
+ WHERE member.id = x.member_id
+;
