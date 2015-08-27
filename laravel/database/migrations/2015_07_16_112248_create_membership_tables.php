1757b11aa99da5a3acd6596f9574a08d081d5b78
sdaicz <sdaicz@klerede.com>
2015-08-26 21:53:40 -0300

    Tables to store members and membership information. re #38

---
+++ laravel/database/migrations/2015_07_16_112248_create_membership_tables.php
@@ -0,0 +1,101 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMembershipTables extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('member', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('venue_id');
+			$table->string('code');
+			$table->integer('last_membership_id');
+			$table->timestamps();
+			$table->unique(['venue_id', 'code']);
+		});
+		Schema::create('membership', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('venue_id');
+			$table->integer('member_id');
+			$table->integer('member_name_id');
+			$table->integer('member_address_id');
+			$table->string('code');
+			$table->integer('sequence');
+			$table->integer('box_office_product_id');
+			$table->date('date_from');
+			$table->date('date_to');
+			$table->date('dob')->nullable();
+			$table->integer('adult_qty');
+			$table->integer('child_qty');
+			$table->timestamps();
+			$table->unique(['venue_id', 'sequence']);
+		});
+		Schema::create('member_name', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('first');
+			$table->string('middle');
+			$table->string('last');
+			$table->timestamps();
+			$table->unique(['first', 'middle', 'last']);
+		});
+		Schema::create('member_address', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('street_1');
+			$table->string('street_2');
+			$table->string('city');
+			$table->string('state');
+			$table->string('country');
+			$table->string('phone');
+			$table->timestamps();
+			$table->index(['street_1', 'street_2', 'city']);
+		});
+		Schema::create('membership_galaxy', function(Blueprint $table)
+		{
+			$table->integer('venue_id');
+			$table->string('member_code');
+			$table->string('code');
+			$table->integer('sequence');
+			$table->string('box_office_product_code');
+			$table->date('date_from');
+			$table->date('date_to');
+			$table->date('dob')->nullable();
+			$table->integer('adult_qty');
+			$table->integer('child_qty');
+			$table->string('first');
+			$table->string('middle');
+			$table->string('last');
+			$table->string('street_1');
+			$table->string('street_2');
+			$table->string('city');
+			$table->string('state');
+			$table->string('country');
+			$table->string('phone');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('member');
+		Schema::dropIfExists('membership');
+		Schema::dropIfExists('member_name');
+		Schema::dropIfExists('member_address');
+		Schema::dropIfExists('membership_galaxy');
+	}
+}
