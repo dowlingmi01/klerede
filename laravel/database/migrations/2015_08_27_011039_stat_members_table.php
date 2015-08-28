ead802c962f83d962b0a0ed18918f34c8b060a64
sdaicz <sdaicz@klerede.com>
2015-08-27 22:17:17 -0300

    Add table for storing membership statistics. re #137

---
+++ laravel/database/migrations/2015_08_27_011039_stat_members_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class StatMembersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('stat_members', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('venue_id');
+			$table->date('date');
+			$table->integer('current_members');
+			$table->integer('returning_members');
+			$table->double('frequency');
+			$table->double('recency');
+			$table->timestamps();
+			$table->unique(['venue_id', 'date']);
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('stat_members');
+	}
+}
