aa78fd97025008a53ac8ed22da7e7e8a47919196
Taylor Johnson <webninjataylor@gmail.com>
2015-08-29 11:53:17 -0400

    Added light and extra-bold font weights, and adjusted stats weight.

--- laravel/resources/views/layout.blade.php
+++ laravel/resources/views/layout.blade.php
@@ -2,7 +2,7 @@
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
         <link rel="stylesheet" href="css/app.css">
-        <link href="//fonts.googleapis.com/css?family=Raleway:400,500,600,700" rel="stylesheet" type="text/css">
+        <link href="//fonts.googleapis.com/css?family=Raleway:300,400,500,600,700,800" rel="stylesheet" type="text/css">
     </head>
     <body>
         <header class="container-fluid">
