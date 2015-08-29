7105b1fc374ef3487d922f9cb0b398c4ce637492
Taylor Johnson <webninjataylor@gmail.com>
2015-08-28 21:05:56 -0400

    Pulling required fields from waether API.

--- laravel/resources/js/app/revenue.js
+++ laravel/resources/js/app/revenue.js
@@ -3,14 +3,41 @@
 /*****************************/
 
 var Revenue = React.createClass({
-    render: function() {
+    getInitialState: function() {
+        return {
+            iconDomain: 'http://openweathermap.org/img/w/',
+            iconFilename: '',   // e.g. 01d (add domain, path, and .png)
+            iconExtension: '.png',
+            temp: '',   // round to a whole number and add symbol and 'F' ... Math.round(78.4); // 78
+            description: ''
+        };
+    },
+    componentDidMount: function() {
         $.get('http://api.openweathermap.org/data/2.5/weather', {
             APPID: '86376bb7c673c089067f51ae70a6e79e',
-            id: '524901'
+            units: 'imperial',
+            zip: '20132,us'   // TO DO: Pull from venue address
         })
         .done(function(result) {
-            console.log(result.weather[0].description);
+            wnt.weather = result;
+            if(this.isMounted()) {
+                this.setState({
+                    iconFilename: result.weather[0].icon,
+                    temp: Math.round(result.main.temp),
+                    description: result.weather[0].description
+                });
+            }
+            console.log('Weather data loaded...');
+            console.log(this.state.iconFilename);
+            console.log(this.state.temp);
+            console.log(this.state.description);
+        }.bind(this))   // .bind() gives context to 'this'
+        .fail(function(result) {
+            console.log('WEATHER DATA ERROR!');
+            console.log(result);
         });
+    },
+    render: function() {
         return (
             <div>
                 TEMP REVENUE ROW
