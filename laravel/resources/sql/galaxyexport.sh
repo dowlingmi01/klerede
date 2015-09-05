a7dcd710c4feb55539ae3e99a308bcc3556d7e6a
sdaicz <sdaicz@klerede.com>
2015-09-05 16:37:39 -0300

    Add member data to export from Galaxy script. re #39

--- laravel/resources/sql/galaxyexport.sh
+++ laravel/resources/sql/galaxyexport.sh
@@ -2,4 +2,5 @@ bcp 'SELECT * FROM export..transaction_header ORDER BY source_id' queryout tranh
 bcp 'SELECT * FROM export..transaction_line ORDER BY source_id, sequence' queryout tranline.txt -cT -d export
 bcp 'SELECT * FROM export..item ORDER BY code' queryout item.txt -cT -d export
 bcp 'SELECT * FROM export..visit ORDER BY source_id' queryout visit.txt -cT -d export
+bcp 'SELECT * FROM export..member ORDER BY code' queryout member.txt -cT -d export
 bcp 'SELECT * FROM export..membership ORDER BY sequence' queryout membership.txt -cT -d export
