812266e8ba264c5ccd6245fd07c5fd469f3a2fe0
sdaicz <sdaicz@klerede.com>
2015-09-15 19:26:48 -0300

    Add tables for supporting the import of store sales from Galaxy data. re #39

--- laravel/database/migrations/2015_03_31_143325_create_store_transaction_table.php
+++ laravel/database/migrations/2015_03_31_143325_create_store_transaction_table.php
@@ -31,6 +31,15 @@ class CreateStoreTransactionTable extends Migration {
 			$table->mediumText('source_xml')->nullable();
 			$table->timestamps();
 		});
+		Schema::create('store_transaction_galaxy_member_info', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('venue_id');
+			$table->integer('register_id');
+			$table->integer('sequence');
+			$table->string('member_code');
+			$table->unique(['venue_id', 'register_id', 'sequence'], 'store_transaction_galaxy_member_info_unique');
+		});
 		Schema::create('store_transaction_line', function(Blueprint $table)
 		{
 			$table->increments('id');
@@ -43,6 +52,18 @@ class CreateStoreTransactionTable extends Migration {
 			$table->text('source_xml')->nullable();
 			$table->timestamps();
 		});
+		Schema::create('store_transaction_line_galaxy', function(Blueprint $table)
+		{
+			$table->integer('venue_id');
+			$table->integer('register_id');
+			$table->integer('store_transaction_sequence');
+			$table->integer('sequence');
+			$table->string('store_product_scanned_code');
+			$table->double('sale_price');
+			$table->integer('quantity');
+			$table->timestamps();
+			$table->primary(['venue_id', 'register_id', 'store_transaction_sequence', 'sequence'], 'store_transaction_line_galaxy_pk');
+		});
 		Schema::create('store_product', function(Blueprint $table)
 		{
 			$table->increments('id');
@@ -51,6 +72,15 @@ class CreateStoreTransactionTable extends Migration {
 			$table->string('description');
 			$table->string('scanned_code');
 			$table->timestamps();
+		});
+		Schema::create('store_product_galaxy', function(Blueprint $table)
+		{
+			$table->integer('venue_id');
+			$table->integer('store_product_category_source_id');
+			$table->string('code');
+			$table->string('description');
+			$table->string('scanned_code');
+			$table->unique(['venue_id', 'scanned_code']);
 		});
 		Schema::create('store_product_category', function(Blueprint $table)
 		{
@@ -82,6 +112,35 @@ class CreateStoreTransactionTable extends Migration {
 			['code'=>'STA', 'description'=>'Stationery'],
 			['code'=>'TOY', 'description'=>'Toy']
 		]);
+		Schema::create('store_product_category_galaxy', function(Blueprint $table)
+		{
+			$table->integer('venue_id');
+			$table->increments('source_id');
+			$table->string('code');
+			$table->unique(['venue_id', 'source_id']);
+		});
+		DB::table('store_product_category_galaxy')->insert([
+			['venue_id'=>1588, 'source_id'=>110, 'code'=>'ACA'],
+			['venue_id'=>1588, 'source_id'=>111, 'code'=>'ACY'],
+			['venue_id'=>1588, 'source_id'=>115, 'code'=>'APA'],
+			['venue_id'=>1588, 'source_id'=>116, 'code'=>'APY'],
+			['venue_id'=>1588, 'source_id'=>120, 'code'=>'BKA'],
+			['venue_id'=>1588, 'source_id'=>121, 'code'=>'BKY'],
+			['venue_id'=>1588, 'source_id'=>125, 'code'=>'CDY'],
+			['venue_id'=>1588, 'source_id'=>130, 'code'=>'CON'],
+			['venue_id'=>1588, 'source_id'=>135, 'code'=>'GEO'],
+			['venue_id'=>1588, 'source_id'=>140, 'code'=>'HOM'],
+			['venue_id'=>1588, 'source_id'=>145, 'code'=>'JYA'],
+			['venue_id'=>1588, 'source_id'=>146, 'code'=>'JYY'],
+			['venue_id'=>1588, 'source_id'=>150, 'code'=>'MED'],
+			['venue_id'=>1588, 'source_id'=>155, 'code'=>'OUT'],
+			['venue_id'=>1588, 'source_id'=>160, 'code'=>'PLT'],
+			['venue_id'=>1588, 'source_id'=>165, 'code'=>'PLU'],
+			['venue_id'=>1588, 'source_id'=>170, 'code'=>'PST'],
+			['venue_id'=>1588, 'source_id'=>175, 'code'=>'SOV'],
+			['venue_id'=>1588, 'source_id'=>180, 'code'=>'STA'],
+			['venue_id'=>1588, 'source_id'=>185, 'code'=>'TOY'],
+		]);
 		Schema::create('member_xstore', function(Blueprint $table)
 		{
 			$table->increments('id');
@@ -109,9 +168,13 @@ class CreateStoreTransactionTable extends Migration {
 	public function down()
 	{
 		Schema::dropIfExists('store_transaction_line');
+		Schema::dropIfExists('store_transaction_line_galaxy');
 		Schema::dropIfExists('store_transaction');
+		Schema::dropIfExists('store_transaction_galaxy_member_info');
 		Schema::dropIfExists('store_product');
+		Schema::dropIfExists('store_product_galaxy');
 		Schema::dropIfExists('store_product_category');
+		Schema::dropIfExists('store_product_category_galaxy');
 		Schema::dropIfExists('store_product_category_group');
 		Schema::dropIfExists('store_register');
 		Schema::dropIfExists('member_xstore');
