714fd559784d41975e937bf94198fb4f0e57529a
Elizabeth Johnson <libbykjohnson@gmail.com>
2016-02-28 14:26:34 -0500

    153 - Manage Users Modal initial setup.

--- laravel/resources/views/settings.blade.php
+++ laravel/resources/views/settings.blade.php
@@ -13,7 +13,7 @@
 <div id="settings-page" class="page">
 
 	<div class="content-box">
-		
+
 		<div class="image-and-text">
 			<div class="user-image"></div>
 			<div class="user-image-text">UPLOAD A PHOTO</div>
@@ -52,11 +52,104 @@
 
 			<div class="setting-title">Users:</div>
 			<div class="users-content">4 Users</div>
-			<a href="????">Manage</a>
+			<a href="#" data-toggle="modal" data-target="#myModal" onclick="return false;">Manage</a>
 			<hr>
-		</div> <!-- .content-box --!>
-	</div> <!-- .settings-box --!>
+		</div> <!-- .content-box -->
+	</div> <!-- .settings-box -->
 
-</div> <!--  #settings-page  --!>
+</div> <!--  #settings-page  -->
+
+
+
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">MANAGE USERS</h4>
+      </div>
+      <div class="modal-body">
+        <div class="manage-users-modal">
+
+				<div class="modal-user-name">Jane Wynter</div>
+				<div class="modal-user-email">Jane@livingplanet.com</div>
+				<div class="modal-acct-type">
+					<div class="dropdown">
+					  <button id="dLabel" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					    Admin
+					    <span class="caret"></span>
+					  </button>
+					  <ul class="dropdown-menu" aria-labelledby="dLabel">
+					    Basic</br>
+					    Editor
+					  </ul>
+					</div>
+				</div>
+				<button class="modal-remove-button">REMOVE</button>
+				<hr>
+				<div class="modal-user-name">Brad Jensen</div>
+				<div class="modal-user-email">Brad@livingplanet.com</div>
+				<div class="modal-acct-type">
+					<div class="dropdown">
+					  <button id="dLabel" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					    Basic
+					    <span class="caret"></span>
+					  </button>
+					  <ul class="dropdown-menu" aria-labelledby="dLabel">
+					    Admin</br>
+					    Editor
+					  </ul>
+					</div>
+				</div>
+				<button class="modal-remove-button">REMOVE</button>
+				<hr>
+				<div class="modal-user-name">Joel Barrett</div>
+				<div class="modal-user-email">Joel@livingplanet.com</div>
+				<div class="modal-acct-type">
+					<div class="dropdown">
+					  <button id="dLabel" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					    Editor
+					    <span class="caret"></span>
+					  </button>
+					  <ul class="dropdown-menu" aria-labelledby="dLabel">
+					    Basic</br>
+						Admin
+					  </ul>
+					</div>
+				</div>
+				<button class="modal-remove-button">REMOVE</button>
+				<hr>
+				<div class="modal-user-name">Amit Sachdeva</div>
+				<div class="modal-user-email">Amit@livingplanet.com</div>
+				<div class="modal-acct-type">
+					<div class="dropdown">
+					  <button id="dLabel" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					    Admin
+					    <span class="caret"></span>
+					  </button>
+					  <ul class="dropdown-menu" aria-labelledby="dLabel">
+					    Basic</br>
+					    Editor
+					  </ul>
+					</div>
+				</div>
+				<button class="modal-remove-button">REMOVE</button>
+				<hr>
+
+
+			<h4 class="modal-title">ADD USERS</h4>
+
+		</div><!-- .manage-users-modal -->
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
 
 @stop
