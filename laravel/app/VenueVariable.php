e8d51c3d14047a43ef55f8520e15ee1d330fc1d4
sdaicz <sdaicz@klerede.com>
2016-03-10 19:59:40 -0300

    Create tables and models for storing import queries to execute and variables to substitute. re #115

---
+++ laravel/app/VenueVariable.php
@@ -0,0 +1,15 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class VenueVariable extends Model {
+	protected $table = 'venue_variable';
+	static function substituteFor($venue_id, $string) {
+		$variables = VenueVariable::where('venue_id', $venue_id)->get();
+		$substs = [];
+		foreach($variables as $variable) {
+			$substs['%'.$variable->name.'%'] = $variable->value;
+		}
+		return strtr($string, $substs);
+	}
+}
