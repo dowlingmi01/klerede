1277105fc4b7d784798d9925b4521cffa1b3f247
sdaicz <sdaicz@klerede.com>
2016-03-10 19:54:15 -0300

    Change qualified class names to use clauses.

--- laravel/app/Http/routes.php
+++ laravel/app/Http/routes.php
@@ -11,7 +11,9 @@
 |
 */
 
+use App\GoalsSales;
 use App\StoreTransaction, App\Stats;
+use App\WeatherDaily;
 
 Route::get('/', 'WelcomeController@index');
 
@@ -39,16 +41,16 @@ Route::group(['prefix'=>'api/v1'], function() {
 	});
 	Route::get('weather/query', function() {
 		$input = (object) Request::all();
-		$result = \App\WeatherDaily::queryD($input);
+		$result = WeatherDaily::queryD($input);
 		return Response::json($result);
 	});
 	Route::get('goals/sales/{venue_id}/{year}', function($venue_id, $year) {
-		return Response::json(\App\GoalsSales::get($venue_id, $year));
+		return Response::json(GoalsSales::get($venue_id, $year));
 	});
 	Route::put('goals/sales/{venue_id}/{year}/{channel}/{type}/{sub_channel?}',
 		function($venue_id, $year, $channel, $type, $sub_channel = null) {
 		$months = Request::input('months');
-		return Response::json(\App\GoalsSales::set($venue_id, $year, $channel, $type, $sub_channel, $months));
+		return Response::json(GoalsSales::set($venue_id, $year, $channel, $type, $sub_channel, $months));
 	});
 });
 
