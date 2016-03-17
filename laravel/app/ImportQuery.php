557a97aa7765790a9368a2281d5c45e788c9f668
sdaicz <sdaicz@klerede.com>
2016-03-17 13:57:40 -0300

    Uncompress query result files upon reception. re #115

--- laravel/app/ImportQuery.php
+++ laravel/app/ImportQuery.php
@@ -1,14 +1,31 @@
 <?php namespace App;
 
+use App\Commands\ProcessImportQuery;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Bus\DispatchesCommands;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Process\Process;
 
 class ImportQuery extends Model {
+	use DispatchesCommands;
 	protected $table = 'import_query';
-	function received($file) {
-		$file->move(storage_path('app/import_query'), $this->id . '.gz');
+	static function getGZDir() {
+		return storage_path('app/import_query');
+	}
+	function getGZPath() {
+		return self::getGZDir() . '/' . $this->id . '.gz';
+	}
+	function received(UploadedFile $file) {
+		$file->move(self::getGZDir(), $this->id . '.gz');
 		$this->status = 'executed';
 		$this->time_executed = Carbon::now();
 		$this->save();
+		$this->dispatch(new ProcessImportQuery($this));
+	}
+	function process() {
+		$cmd = 'gzip -d ' . $this->getGZPath();
+		$process = new Process($cmd);
+		$process->run();
 	}
 }
