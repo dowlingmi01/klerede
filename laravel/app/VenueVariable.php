e12c0f0a91e8ce1e7be134c4e1f13fc2692e7264
sdaicz <sdaicz@klerede.com>
2016-04-28 11:26:11 -0300

    Add function for setting substitution variables. re #115

--- laravel/app/VenueVariable.php
+++ laravel/app/VenueVariable.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Eloquent\Model;
 
 class VenueVariable extends Model {
+	protected $guarded = [];
 	protected $table = 'venue_variable';
 	static function substituteFor($venue_id, $string) {
 		$variables = VenueVariable::where('venue_id', $venue_id)->get();
@@ -11,5 +12,10 @@ class VenueVariable extends Model {
 			$substs['#'.$variable->name.'#'] = $variable->value;
 		}
 		return strtr($string, $substs);
+	}
+	static function setValue($venue_id, $name, $value) {
+		$var = self::firstOrNew(['venue_id'=>$venue_id, 'name'=>$name]);
+		$var->value = $value;
+		$var->save();
 	}
 }
