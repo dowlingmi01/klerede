9f1a83fed18d6e79f9d17a82abd36094a3d2b81a
sdaicz <sdaicz@klerede.com>
2016-05-05 21:21:20 -0300

    Move parameters of Powershell script to configuration file. re #115

--- laravel/resources/bin/klerede-import.ps1
+++ laravel/resources/bin/klerede-import.ps1
@@ -1,25 +1,41 @@
 . "$PSScriptRoot\util.ps1"
 
-$database = 'Galaxy1'
-$venue_id = 1588
-$baseUrl = 'http://local.kl/api/v1'
-$queryUrl = '?XDEBUG_SESSION_START=session_name'
-$urlQuery = "$baseUrl/import/query$queryUrl"
-$urlUpload = "$baseUrl/import/query-result$queryUrl"
-
 function Get-Query($venue_id, $last_query_id) {
-    Invoke-RestMethod -Uri $urlQuery -Body @{"venue_id"=$venue_id; "last_query_id"=$last_query_id} -Verbose
+    Invoke-RestMethod -Uri $url_query -Body @{"venue_id"=$venue_id; "last_query_id"=$last_query_id} -Verbose
 }
 function Run-Query($query_text, $file_name) {
-    & bcp $query_text queryout $file_name -cT -d $database
+    if($cfg_sql_auth -eq 'trusted') {
+        & bcp $query_text queryout $file_name -c -d $cfg_sql_database -S $cfg_sql_server -T
+    } else {
+        & bcp $query_text queryout $file_name -c -d $cfg_sql_database -S $cfg_sql_server -U $cfg_sql_user -P $cfg_sql_password
+    }
 }
 function Upload-Result($url, $file_name) {
     $wc = new-object System.Net.WebClient
     [System.Text.Encoding]::ASCII.GetString($wc.UploadFile( $url, $file_name )) | ConvertFrom-Json
 }
 
+$conf_file = 'klerede-import.ini'
+
+# Read the contents of the configuration file into the variables.
+Try {
+	Get-Content $conf_file -ErrorAction Stop | Foreach-Object {
+		$var = $_.Split('=')
+		Set-Variable -Name cfg_$($var[0]) -Value $var[1]
+	}
+} Catch {
+	$message = "Could not read configuration file $conf_file`r`n"
+	$message = $message + $_.Exception.Message
+	$message = $message + $_.Exception.StackTrace
+	Write-Error $message
+    exit 1
+}
+
+$url_query = "$cfg_base_url/api/v1/import/query"
+$url_upload = "$cfg_base_url/api/v1/import/query-result"
+
 $last_query_id = 0
-$resultQ = Get-Query $venue_id $last_query_id
+$resultQ = Get-Query $cfg_venue_id $last_query_id
 
 while($resultQ.query_id -gt 0) {
     $fileName = [System.IO.Path]::GetTempFileName()
@@ -29,9 +45,9 @@ while($resultQ.query_id -gt 0) {
     Compress-GZip $fileName $fileNameComp
     Remove-Item $fileName
 
-    Upload-Result "$urlUpload&query_id=$($resultQ.query_id)" $fileNameComp
+    Upload-Result "${url_upload}?query_id=$($resultQ.query_id)" $fileNameComp
     Remove-Item $fileNameComp
 
     $last_query_id = $resultQ.query_id
-    $resultQ = Get-Query $venue_id $last_query_id
+    $resultQ = Get-Query $cfg_venue_id $last_query_id
 }
