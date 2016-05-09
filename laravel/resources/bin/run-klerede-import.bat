d895c58974fc32b3494d95e3d0cf3670fb23f8cb
sdaicz <sdaicz@klerede.com>
2016-05-09 11:36:04 -0300

    Create log dir if it doesn't exist in batch file for running Powershell. re #115

--- laravel/resources/bin/run-klerede-import.bat
+++ laravel/resources/bin/run-klerede-import.bat
@@ -1,5 +1,6 @@
 @echo off
 FOR /F "delims=" %%i IN ('powershell -Command Get-Date -Format yyyy-MM-ddThhmmss') DO SET tms=%%i
 set cdir=%~dp0%
-set pth=%cdir%log\%tms%.log
-powershell -ExecutionPolicy RemoteSigned -File klerede-import.ps1 2>&1 >> %pth%
+set pth=%cdir%log
+if not exist %pth% mkdir %pth%
+echo powershell -ExecutionPolicy RemoteSigned -File klerede-import.ps1 2>&1 >> %pth%\%tms%.log
