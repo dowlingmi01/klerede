92b952f04395abbb152593a2fac5294633ca9ba8
sdaicz <sdaicz@klerede.com>
2016-05-09 11:38:18 -0300

    Remove test code from batch file for running Powershell. re #115

--- laravel/resources/bin/run-klerede-import.bat
+++ laravel/resources/bin/run-klerede-import.bat
@@ -3,4 +3,4 @@ FOR /F "delims=" %%i IN ('powershell -Command Get-Date -Format yyyy-MM-ddThhmmss
 set cdir=%~dp0%
 set pth=%cdir%log
 if not exist %pth% mkdir %pth%
-echo powershell -ExecutionPolicy RemoteSigned -File klerede-import.ps1 2>&1 >> %pth%\%tms%.log
+powershell -ExecutionPolicy RemoteSigned -File klerede-import.ps1 2>&1 >> %pth%\%tms%.log
