62664ea8f91dddab78b7263d71853d7a9f947bba
Elizabeth Johnson <libbykjohnson@gmail.com>
2016-05-16 09:13:07 -0400

    153 - Manage Users styling and integration points.

--- laravel/resources/js/app/header.js
+++ laravel/resources/js/app/header.js
@@ -1,6 +1,63 @@
-/*******************************************************************/
-/******** WELCOME TEXT WITH USER'S NAME AND LOCAL TIME/DATE ********/
-/*******************************************************************/
+/************************/
+/******** HEADER ********/
+/************************/
+
+var User = React.createClass({
+    getInitialState: function() {
+        return {
+        };
+    },
+    userAccordion: function(event){
+        // Set index of user in list to use in jQuery addClass after component is updated
+        this.setState({
+            editUserIndex: $('.user').index($(event.target).closest('.user'))
+        });
+    },
+    deleteUser: function(){
+        event.preventDefault();
+        // TO DO: Delete user code
+        console.log('DELETE USER', this.state);
+        event.target.blur();
+    },
+    passwordReset: function(){
+        event.preventDefault();
+        // TO DO: Change password code
+        this.setState({ message: 'An email has been sent to the user to reset their password.' });
+        console.log('CHANGE PASSWORD', this.state);
+        event.target.blur();
+    },
+    componentDidUpdate: function(){
+        // jQuery can control classes after components update
+        $('.user').eq(this.state.editUserIndex).toggleClass('active');
+    },
+    render: function() {
+        return (
+            <div className="user" onClick={this.userAccordion}>
+                {this.props.name} <Caret className="utilities-caret" />
+                <div className="quick-edit stop">
+                    <div className="email">{this.props.email}</div>
+                    <Roles />
+                    <a className="stop" onClick={this.deleteUser}>Delete User</a>
+                    <a className="stop" onClick={this.passwordReset}>Password Reset</a>
+                    <div className="message">{this.state.message}</div>
+                </div>
+            </div>
+        );
+    }
+});
+
+var Roles = React.createClass({
+    render: function() {
+        return (
+            <select className="form-control stop" id={this.props.id}>
+                <option value="basic">Basic</option>
+                <option value="power">Power</option>
+                <option value="admin">Admin</option>
+                <option value="owner">Owner</option>
+            </select>
+        );
+    }
+});
 
 var Header = React.createClass({
     getInitialState: function() {
@@ -16,7 +73,9 @@ var Header = React.createClass({
             planTitle: 'Professional',
             planDescription: 'Unlimited Accounts',
             cardName: 'Dan Tribec',
-            cardNumber: '**** **** **** 1234'
+            cardNumber: '**** **** **** 1234',
+            users: ['Michael Dowling', 'Matt Pelletier', 'Heather Finney', 'Sergio Daicz', 'Taylor Johnson', 'Elizabeth Johnson'],
+            usersEmail: ['michael@klerede.com', 'matt@klerede.com', 'hfinney@klerede.com', 'sdaicz@klerede.com', 'webninjataylor@gmail.com', 'libbykjohsnon@gmail.com']
         };
     },
     toggleSettings: function() {
@@ -64,6 +123,9 @@ var Header = React.createClass({
         if(field === 'pwdMatch'){
             console.log('PWD MATCH?!');
         }
+        if(($('#fName').val() !== '') && ($('#lName').val() !== '') && ($('#email').val() !== '')){
+            $('#manage-users').find('.disabled').removeClass('disabled');
+        }
     },
     saveChanges: function(event){
         event.preventDefault();
@@ -71,10 +133,24 @@ var Header = React.createClass({
         console.log('SAVE', this.state);
         event.target.blur();
     },
+    addUser: function(event){
+        event.preventDefault();
+        // TO DO: Send changed data to the API
+        this.setState({addUserRole: $('#addUserRole').val() });
+        console.log('ADD USER', this.state);
+        event.target.blur();
+    },
     changePlan: function(event){
         event.preventDefault();
         // TO DO: Write plan change code
         console.log('CHANGE PLAN', this.state);
+        $('#change-plan').addClass('active');
+        event.target.blur();
+    },
+    choosePlan: function(event){
+        event.preventDefault();
+        // TO DO: Write plan change code
+        console.log('CHOOSE PLAN', this.state);
         event.target.blur();
     },
     cancelPlan: function(event){
@@ -96,6 +172,11 @@ var Header = React.createClass({
         event.target.blur();
     },
     render: function() {
+        // LOOP FOR BAR SETS
+        var users = [];
+        for (var i = 0; i < this.state.users.length; i++) {
+            users.push(<User name={this.state.users[i]} email={this.state.usersEmail[i]} />);
+        }
         return (
             <header className="container-fluid">
                 <div className="row">
@@ -140,7 +221,7 @@ var Header = React.createClass({
                     <div className="utility" data-utility="logout" data-type="tbd">Logout</div>
                 </div>
                 <div id="user-profile" className="utilities-set" onClick={this.activateField}>
-                    <h3>User Profile <div className="glyphicon glyphicon-remove close" id="close-profile" aria-hidden="true" onClick={this.closeUtility}></div></h3>
+                    <h3>User Profile <div className="glyphicon glyphicon-remove close" aria-hidden="true" onClick={this.closeUtility}></div></h3>
                     <form className="settings">
                         <div className="form-group">
                             <label htmlFor="up-name">Full Name:</label>
@@ -176,19 +257,27 @@ var Header = React.createClass({
                     </form>
                 </div>
                 <div id="manage-users" className="utilities-set" onClick={this.activateField}>
-                    <h3>Manage Users <div className="glyphicon glyphicon-remove close" id="close-users" aria-hidden="true" onClick={this.closeUtility}></div></h3>
-                    <form className="settings">
-                        <div className="form-group">
-                            <label htmlFor="mu-add">Add a User:</label>
-                            <input type="text" id="mu-add" />
-                        </div>
-                        <div className="form-group">
-                            <input type="submit" value="Save User" className="btn" />
-                        </div>
+                    <h3>Manage Users <div className="glyphicon glyphicon-remove close" aria-hidden="true" onClick={this.closeUtility}></div></h3>
+                    <div className="utility-group">
+                        {users}
+                    </div>
+                    <form className="utility-group form-group">
+                        <h4>Add a User</h4>
+                        <input type="text" id="fName" className="form-control" placeholder="First Name" value={this.state.addUserFirstName} data-field="addUserFirstName" onChange={this.changeField} />
+                        <input type="text" id="lName" className="form-control" placeholder="Last Name" value={this.state.addUserLastName} data-field="addUserLastName" onChange={this.changeField} />
+                        <input type="text" id="email" className="form-control" placeholder="Email Address" value={this.state.addUserEmail} data-field="addUserEmail" onChange={this.changeField} />
+                        <Roles id="addUserRole" />
+                        <input type="submit" value="Save User" className="btn disabled" onClick={this.addUser} />
                     </form>
+                    <div className="utility-group" onClick={this.toggleUtility}>
+                        <div className="utility sub-item" data-utility="user-types" data-type="modal">
+                            Learn about user types
+                            <Caret className="utilities-caret" />
+                        </div>
+                    </div>
                 </div>
                 <div id="account-settings" className="utilities-set" onClick={this.activateField}>
-                    <h3>Account Settings <div className="glyphicon glyphicon-remove close" id="close-accountsettings" aria-hidden="true" onClick={this.closeUtility}></div></h3>
+                    <h3>Account Settings <div className="glyphicon glyphicon-remove close" aria-hidden="true" onClick={this.closeUtility}></div></h3>
                     <div className="utility-group">
                         <h4>Your Plan</h4>
                         <div className="plan-title">{this.state.planTitle}</div>
@@ -208,6 +297,44 @@ var Header = React.createClass({
                         <a href="#" className="invoice" onClick={this.openInvoice}>April 3, 2016 <span className="amount">$540.00</span></a>
                         <a href="#" className="invoice" onClick={this.openInvoice}>March 3, 2016 <span className="amount">$540.00</span></a>
                         <a href="#" className="invoice" onClick={this.openInvoice}>February 3, 2016 <span className="amount">$540.00</span></a>
+                    </div>
+                </div>
+                <div id="user-types" className="utilities-set">
+                    <h3>User Types <div className="glyphicon glyphicon-remove close" aria-hidden="true" onClick={this.closeUtility}></div></h3>
+                    <div className="utility-group">
+                        <div className="sub-item">
+                            <h4>Owner</h4>
+                            <p>The owner serves as the primary contact for the account. Owners have access to billing and plan information.</p>
+                        </div>
+                        <div className="sub-item">
+                            <h4>Admin</h4>
+                            <p>The admin can perform all actions in the account; including adding new users, closing accounts, and setting and editing goals.</p>
+                        </div>
+                        <div className="sub-item">
+                            <h4>Power</h4>
+                            <p>The power user can set and edit goals, view and export reports, but can&quot;t view billing information, add users, or close accounts.</p>
+                        </div>
+                        <div className="sub-item">
+                            <h4>Basic</h4>
+                            <p>The basic user can view the dashboard, and export reports.</p>
+                        </div>
+                    </div>
+                </div>
+                <div id="change-plan" className="utilities-set">
+                    <h3>Time for an upgrade! <div className="glyphicon glyphicon-remove close" aria-hidden="true" onClick={this.closeUtility}></div></h3>
+                    <div className="utility-group">
+                        <div className="sub-item">
+                            <h4>Professional</h4>
+                            <p>Unlimited Accounts <div className="pricing">$540/yr $50/mo</div> <div className="plan-action">Current Plan</div></p>
+                        </div>
+                        <div className="sub-item">
+                            <h4>Individual</h4>
+                            <p>Advanced Features <div className="pricing">$440/yr $40/mo</div> <div className="plan-action"><button className="btn" onClick={this.choosePlan}>Choose Plan</button></div></p>
+                        </div>
+                        <div className="sub-item">
+                            <h4>Enterprise</h4>
+                            <p>Advanced Features <div className="pricing">$640/yr $60/mo</div> <div className="plan-action"><button className="btn" onClick={this.choosePlan}>Choose Plan</button></div></p>
+                        </div>
                     </div>
                 </div>
             </header>
