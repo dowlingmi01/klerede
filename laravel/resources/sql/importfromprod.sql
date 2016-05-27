f9c26d8502dddefb58d9144b4ffa80302ae0955a
sdaicz <sdaicz@klerede.com>
2016-05-27 18:24:30 -0300

    Fix script for importing stats from prod db. re #115

--- laravel/resources/sql/importfromprod.sql
+++ laravel/resources/sql/importfromprod.sql
@@ -12,7 +12,12 @@ DELETE FROM stat_sales
    AND date > '2015-01-28'
 ;
 INSERT stat_sales
-SELECT * FROM klerede_prod.stat_sales
+     ( venue_id, date, year, quarter, month, week, channel_id, box_office_product_kind_id, membership_kind_id
+     , members, online, units, amount, created_at, updated_at
+     )
+SELECT venue_id, date, year, quarter, month, week, channel_id, box_office_product_kind_id, membership_kind_id
+     , members, online, units, amount, created_at, updated_at
+  FROM klerede_prod.stat_sales
  WHERE venue_id = 1588
    AND date > '2015-01-28'
 ;
