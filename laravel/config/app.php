fbb4fd56df9096aa9484fc99554972d180134876
sdaicz <sdaicz@klerede.com>
2016-08-09 13:39:13 -0300

    Some recommended changes for 5.2 upgrade.

--- laravel/config/app.php
+++ laravel/config/app.php
@@ -149,8 +149,6 @@ return [
 		 * Application Service Providers...
 		 */
 		'App\Providers\AppServiceProvider',
-		//'App\Providers\BusServiceProvider',
-		'App\Providers\ConfigServiceProvider',
 		'App\Providers\EventServiceProvider',
 		'App\Providers\RouteServiceProvider',
 		Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
