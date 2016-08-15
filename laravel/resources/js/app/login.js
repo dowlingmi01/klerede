2ab96646806d2d0c493f0bb77a5d6dba8331a3d2
Carlos Sandoval <salsadesoja@gmail.com>
2016-08-15 17:56:46 -0300

    Reset password step 1 layout ready.

--- laravel/resources/js/app/login.js
+++ laravel/resources/js/app/login.js
@@ -29,9 +29,18 @@ var RememberCheckMark = React.createClass({
 });
 
 var LoginComponent = React.createClass({
+	getInitialState:function () {
+		return {
+			login:'active',
+			reset:'inactive'
+		}
+	},
 	handleKeyPress:function (event) {
+		this.callIfEnter(event, this.submitForm);
+	},
+	callIfEnter:function (event, func) {
 		if (event.charCode == 13) {
-			this.submitForm();
+			func();
 		}
 	},
 	submitForm:function () {
@@ -88,6 +97,32 @@ var LoginComponent = React.createClass({
 			//alert("error imprevisto: " + success);
 		}
 	},
+	showSection:function (section) {
+		var state = this.state;
+		for (var k in state) {
+			if (k == section) {
+				state[k] = 'active';
+			} else {
+				state[k] = 'inactive';
+			}
+		}
+		
+		this.setState(state);
+	},
+	reset:function (event) {
+		event.preventDefault();
+		this.showSection('reset');
+	},
+	handleResetKeyPress:function (event) {
+		this.callIfEnter(event, this.resetSubmit);
+	},
+	resetSubmit:function () {
+		console.log('resetSubmit');
+		//
+	},
+	resetCancel:function () {
+		this.showSection('login');
+	},
 	render:function () {
 		var user = getLocal("user");
 		
@@ -105,9 +140,9 @@ var LoginComponent = React.createClass({
 							Welcome Back to Klerede!
 						</div>
 					</header>
-					<section>
+					<section className={this.state.login}>
 						<div id="title">Sign into your account</div>
-						<div id="login-form" className="form-group col-xs-10 col-xs-offset-1" onKeyPress={this.handleKeyPress} >
+						<div id="login-form" className="form-group col-xs-10 col-xs-offset-1 klerede-form" onKeyPress={this.handleKeyPress} >
 							
 							<input type="text" name="email" ref="email" id="email" placeholder="Email" defaultValue={user.email} />
 							<input type="password" name="password" ref="password" id="password" placeholder="Password" defaultValue={user.password} />
@@ -116,10 +151,23 @@ var LoginComponent = React.createClass({
 									<span id="remember-checkmark"><RememberCheckMark ref='remember-checkmark' active={user.remember} /></span>&nbsp;Remember Me
 								</div>
 								<div className="col-xs-6 text-align-right">
-									Forgot <a href='#'>Password</a>
+									Forgot <a href='#forgotPassword' onClick={this.reset}>Password</a>
 								</div>
 							</div>
 							<button className="btn form-group col-xs-4 col-xs-offset-4" onClick={this.submitForm}>Sign In</button>
+						</div>
+					</section>
+					<section className={this.state.reset}>
+						<div id="title">Reset Password</div>
+						<div id="reset-form" className="form-group col-xs-10 col-xs-offset-1 klerede-form" onKeyPress={this.handleResetKeyPress} >
+							
+							<input type="text" name="email" ref="email" id="email" placeholder="Email" defaultValue={user.email} />
+							<div id="options" className="row">
+								<div className="col-xs-6 col-xs-offset-6 text-align-right">
+									<a href='#cancelReset' onClick={this.resetCancel}>Cancel</a>
+								</div>
+							</div>
+							<button className="btn form-group col-xs-4 col-xs-offset-4" onClick={this.resetSubmit}>Reset</button>
 						</div>
 					</section>
 					<div className="clearfix">
