8659c1c42d59c32a1cd4083d619fe957168d0ccf
sdaicz <sdaicz@klerede.com>
2016-08-20 13:12:46 -0300

    Fix for importing Galaxy box office transactions.

--- laravel/app/ImportQueryHandlers/GalaxyBoxOfficeTransaction.php
+++ laravel/app/ImportQueryHandlers/GalaxyBoxOfficeTransaction.php
@@ -19,7 +19,7 @@ class GalaxyBoxOfficeTransaction extends ImportQueryHandler {
 		$this->addCodes('operator_id', Operator::class);
 		$cols1 = ['t.venue_id', 'source_id', 'register_id', 'sequence', 'business_day', 'time', 'o.id', 't.created_at'];
 		$cols2 = ['venue_id', 'source_id', 'register_id', 'sequence', 'business_day', 'time', 'operator_id', 'created_at'];
-		$sel = DB::table($this->getTableName())->where('query_id', $this->query->id)->select($cols1);
+		$sel = DB::table('import_galaxy_box_office_transaction as t')->where('query_id', $this->query->id)->select($cols1);
 		$sel->join('operator as o', 'operator_id', '=', 'o.code');
 		$sel->where('o.venue_id', $this->query->venue_id);
 		$colsString = implode(', ', $cols2);
