1f536714438b92abd2854ba7e216f17cd2be51b0
Carlos Sandoval <salsadesoja@gmail.com>
2016-08-24 17:02:21 -0300

    Missing file reporting-on.js added.

---
+++ laravel/resources/js/app/reporting-on.js
@@ -0,0 +1,34 @@
+/*******************************************************************/
+/******** WELCOME TEXT WITH USER'S NAME AND LOCAL TIME/DATE ********/
+/*******************************************************************/
+
+var ReportingOn = React.createClass({
+    getInitialState: function() {
+		var date = new Date(wnt.today);
+        return {
+            day:date.getUTCDate(),
+			month:wnt.months[date.getUTCMonth()],
+			year:date.getUTCFullYear()
+        };
+    },
+    render: function() {
+        return (
+			<div>
+				<h4>Reporting On</h4>
+	            <div className='time-date'>
+	                {this.state.day} <span className="month-year">{this.state.month} {this.state.year}</span>
+	            </div>
+			</div>
+        );
+    }
+});
+
+if(document.getElementById('reporting-on')){
+    $.when(wnt.gettingVenueData).done(function(data) {
+		console.log(wnt.today);
+	    React.render(
+	        <ReportingOn />,
+	        document.getElementById('reporting-on')
+	    );
+    });
+}
