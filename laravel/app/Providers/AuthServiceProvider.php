369e29ff059564b1b611901e6732ffd2ced2a90d
Leo Cremona <lcremona@gmail.com>
2016-08-27 14:09:41 -0300

    Gates

--- laravel/app/Providers/AuthServiceProvider.php
+++ laravel/app/Providers/AuthServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Contracts\Auth\Access\Gate as GateContract;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use App\Helpers\PermissionHelper;
+
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,28 @@ class AuthServiceProvider extends ServiceProvider
 
         $gate->define('validate-venue', function ($user, $venue_id) {
             return $user->venue_id == $venue_id;
+        });
+
+        $gate->define('user-get', function ($user, $user_id) {
+            return $user->user_id == $user_id || PermissionHelper::has_permission($user, 'UM');
+        });
+
+        $gate->define('user-set', function ($user, $user_id) {
+            return $user->user_id == $user_id || PermissionHelper::has_permission($user, 'UM');
+        });
+
+        $gate->define('has-permission', function ($user, $permission) {
+            return PermissionHelper::has_permission($user, $permission);
+        });
+
+         $gate->define('valid-role', function ($user, $role_id) {
+
+            $user_role = \App\Role::find($user->role_id);
+            $role = \App\Role::find($role_id);
+            if(!$role || !$user_role)
+                return false;
+
+            return $role->level <= $user_role->level;
         });
     }
  
---
+++ laravel/database/migrations/2016_08_27_155333_add_level_to_role_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddLevelToRoleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('role', function (Blueprint $table) {
+            $table->smallInteger('level')->after('name');
+            $table->string('permission', 256)->after('level');
+        });
+
+ 
+        $roles = array(
+                ['id'=>1, 'name' => 'Owner', 'level'=>'40', 'permission'=>'UM|GS'],
+                ['id'=>2, 'name' => 'Admin', 'level'=>'30', 'permission'=>'UM|GS'],
+                ['id'=>3, 'name' => 'Power', 'level'=>'20', 'permission'=>'GS'],
+                ['id'=>4, 'name' => 'Basic', 'level'=>'10', 'permission'=>''],
+        );
+        DB::table('role')->truncate();
+        DB::table('role')->insert($roles);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('role', function (Blueprint $table) {
+            $table->dropColumn('level');
+            $table->dropColumn('permission');
+        });
+    }
+}
