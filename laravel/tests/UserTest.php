f043ff197f0d830a6b9bd4462796f78f6464f608
Leo Cremona <lcremona@gmail.com>
2016-09-13 17:43:24 -0300

    not working

--- laravel/tests/UserTest.php
+++ laravel/tests/UserTest.php
@@ -3,9 +3,48 @@
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\Model as Eloquent;
 
 class UserTest extends TestCase
 {
+
+
+
+    public static function setUpBeforeClass()
+    {
+        $app->withEloquent();
+        User::with('email', '=', '1518+owner@test.com')->delete();
+        User::with('email', '=', '1588+owner@test.com')->delete();
+        User::with('email', '=', '1518+admin@test.com')->delete();
+        User::with('email', '=', '1588+admin@test.com')->delete();
+
+
+        $users = array(
+                [ 'first_name' => '1518 Test', 'last_name' => 'Owner', 'email' => '1518+owner@test.com', 'password' => Hash::make('secretowner'), 'venue_id' => 1518, 'role_id' => 1],
+                ['first_name' => '1588 Test', 'last_name' => 'Owner', 'email' => '1588+owner@test.com', 'password' => Hash::make('secretowner'), 'venue_id' => 1588, 'role_id' => 1],
+                ['first_name' => '1518 Test', 'last_name' => 'Admin', 'email' => '1518+admin@test.com', 'password' => Hash::make('secretadmin'), 'venue_id' => 1518, 'role_id' => 2],
+                ['first_name' => '1588 Test', 'last_name' => 'Admin', 'email' => '1588+admin@test.com', 'password' => Hash::make('secretadmin'), 'venue_id' => 1588, 'role_id' => 2],
+        );
+
+        // Loop through each user above and create the record for them in the database
+        foreach ($users as $user)
+        {
+            User::create($user);
+        }
+    }
+
+    public static function xtearDownAfterClass(){
+        parent::tearDown();
+         User::with('email', '=', '1518+owner@test.com')->delete();
+        User::with('email', '=', '1588+owner@test.com')->delete();
+        User::with('email', '=', '1518+admin@test.com')->delete();
+        User::with('email', '=', '1588+admin@test.com')->delete();
+
+
+    }
+
     //use DatabaseMigrations;
     /**
      * A basic test example.
@@ -34,7 +73,7 @@ class UserTest extends TestCase
 
 
 
-        $this->json('POST', 'api/v1/auth/login', ['email'=>'aqua+owner@klerede.com','password'=>'secretowner'])
+        $this->json('POST', 'api/v1/auth/login', ['email'=>'1518+owner@test.com','password'=>'secretowner'])
              ->seeJsonStructure([
                  'token'
              ]);
@@ -45,7 +84,7 @@ class UserTest extends TestCase
         //$result = $this->json('POST', 'api/v1/auth/login', ['email'=>'aqua+owner@klerede.com','password'=>'secretowner']);
         
 
-        $result = $this->post('api/v1/auth/login', ['email'=>'aqua+owner@klerede.com','password'=>'secretowner'])
+        $result = $this->post('api/v1/auth/login', ['email'=>'1518+owner@test.com','password'=>'secretowner'])
                 ->seeJsonStructure([
                  'token'
          ]);
@@ -57,8 +96,34 @@ class UserTest extends TestCase
        $token = $responseData['token'];
        
         $this->json('GET', 'api/v1/auth/logged?token='.$token)
-             ->seeJson([
-                 'error'=>'token_not_provided'
+             ->seeJsonStructure([
+                 'user' => [
+                     'id', 'name', 'email'
+                 ]
              ]);
-             }
+    }
+
+    public function testCreateUser()
+    {
+        //$result = $this->json('POST', 'api/v1/auth/login', ['email'=>'aqua+owner@klerede.com','password'=>'secretowner']);
+        
+
+        $result = $this->post('api/v1/auth/login', ['email'=>'1518+owner@test.com','password'=>'secretowner'])
+                ->seeJsonStructure([
+                 'token'
+         ]);
+       
+
+        $token = '';
+        
+       $responseData = json_decode($this->response->getContent(), true);
+       $token = $responseData['token'];
+       
+        $this->json('GET', 'api/v1/auth/logged?token='.$token)
+             ->seeJsonStructure([
+                 'user' => [
+                     'id', 'name', 'email'
+                 ]
+             ]);
+    }
 }
