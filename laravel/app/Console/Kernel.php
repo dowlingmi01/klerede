0588eadfa3a4f555f34c1177e66cb98c6e211a1a
sdaicz <sdaicz@klerede.com>
2016-10-24 20:06:32 -0300

    Add command for creating a user from command line. re #454

--- laravel/app/Console/Kernel.php
+++ laravel/app/Console/Kernel.php
@@ -22,6 +22,7 @@ class Kernel extends ConsoleKernel {
 		'App\Console\Commands\ImportQueryInit',
 		'App\Console\Commands\ImportQueryProcess',
 		'App\Console\Commands\ImportFromProd',
+		'App\Console\Commands\UserCreate',
 	];
 
 	/**
