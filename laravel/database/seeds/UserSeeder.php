9cd0756e01329f13e868b5b822e6d77a308a53a5
sdaicz <sdaicz@klerede.com>
2016-10-24 16:46:45 -0300

    Add default PSC users to seed data. re #454

--- laravel/database/seeds/UserSeeder.php
+++ laravel/database/seeds/UserSeeder.php
@@ -18,8 +18,10 @@ class UserSeeder extends Seeder {
 		$users = array(
                 ['first_name' => 'Aqua Test', 'last_name' => 'Owner', 'email' => 'aqua+owner@klerede.com', 'password' => Hash::make('secretowner'), 'venue_id' => 1518, 'role_id' => 1],
                 ['first_name' => 'LLPA Test', 'last_name' => 'Owner', 'email' => 'llpa+owner@klerede.com', 'password' => Hash::make('secretowner'), 'venue_id' => 1588, 'role_id' => 1],
+                ['first_name' => 'PSC Test', 'last_name' => 'Owner', 'email' => 'psc+owner@klerede.com', 'password' => Hash::make('secretowner'), 'venue_id' => 1204, 'role_id' => 1],
                 ['first_name' => 'Aqua Test', 'last_name' => 'Admin', 'email' => 'aqua+admin@klerede.com', 'password' => Hash::make('secretadmin'), 'venue_id' => 1518, 'role_id' => 2],
                 ['first_name' => 'LLPA Test', 'last_name' => 'Admin', 'email' => 'llpa+admin@klerede.com', 'password' => Hash::make('secretadmin'), 'venue_id' => 1588, 'role_id' => 2],
+                ['first_name' => 'PSC Test', 'last_name' => 'Admin', 'email' => 'psc+admin@klerede.com', 'password' => Hash::make('secretadmin'), 'venue_id' => 1204, 'role_id' => 2],
         );
 
         // Loop through each user above and create the record for them in the database
