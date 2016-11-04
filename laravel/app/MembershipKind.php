4a35d042a6a90533ea45f2a588af25302ef65ca9
sdaicz <sdaicz@klerede.com>
2016-11-04 19:32:04 -0300

    Mark MembershipKind model as not having timestamps.

--- laravel/app/MembershipKind.php
+++ laravel/app/MembershipKind.php
@@ -5,6 +5,7 @@ use Illuminate\Database\Eloquent\Model;
 class MembershipKind extends Model {
 	protected $table = 'membership_kind';
 	protected $guarded = [];
+	public $timestamps = false;
 
 	static function getFor($code) {
 		return self::firstOrCreate(['code'=>$code]);
