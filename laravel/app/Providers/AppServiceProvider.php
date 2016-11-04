9942d49f3cef1694580f8887f50210d09ba0d858
Leo Cremona <lcremona@gmail.com>
2016-11-04 20:54:02 -0300

    date validation

--- laravel/app/Providers/AppServiceProvider.php
+++ laravel/app/Providers/AppServiceProvider.php
@@ -15,7 +15,23 @@ class AppServiceProvider extends ServiceProvider {
 		Validator::extend('before_equal', function($attribute, $value, $parameters, $validator) {
    			  return strtotime($validator->getData()[$parameters[0]]) >= strtotime($value);
 		});
+
+		Validator::extend('long_date_format', function($attribute, $value, $parameters, $validator) {
+			  $format = "Y-m-d H:i:s";
+			  $d = \DateTime::createFromFormat($format, $value);
+   			  return $d && $d->format($format) == $value;
+		});
 	 
+	 	Validator::extend('conditional_time', function($attribute, $value, $parameters, $validator) {
+			  $without_time = filter_var($value, FILTER_VALIDATE_BOOLEAN);
+    		  $zero_time_1 = substr($validator->getData()[$parameters[0]], -8)  === '00:00:00';
+    		  $zero_time_2 = substr($validator->getData()[$parameters[1]], -8)  === '00:00:00';
+   			 
+   			  return (!$without_time && !($zero_time_1 && $zero_time_2))  || ($without_time && $zero_time_1 && $zero_time_2);
+		});
+
+
+
 		Validator::extend('date_time_conditional', function($attribute, $value, $parameters, $validator) {
 			  $without_time = filter_var($validator->getData()[$parameters[0]], FILTER_VALIDATE_BOOLEAN);
    			  $format = "Y-m-d H:i:s"; //$whitout_time ? "Y-m-d  00:00:00" : "Y-m-d H:i:s"   ;
