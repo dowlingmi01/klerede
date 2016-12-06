fd32044abdccd26f4e2d1ba500bfb6fa5e532096
Leo Cremona <lcremona@gmail.com>
2016-12-06 00:01:11 -0300

    note stars or ends between interval

--- laravel/app/Http/Controllers/NoteController.php
+++ laravel/app/Http/Controllers/NoteController.php
@@ -53,10 +53,9 @@ class NoteController extends Controller
 	        $venues[] = $request->venue_id;
         }
        
-                 
         return Note::with(['channels', 'tags'])
-                    ->where('time_start', '>', $request->end)
-                    ->where('time_end', '<', $request->start)
+                    ->where('time_start', '<', $request->end)
+                    ->where('time_end', '>', $request->start)
                     ->whereIn('venue_id', $venues)
                     ->orderBy('time_start')
                     ->get();
