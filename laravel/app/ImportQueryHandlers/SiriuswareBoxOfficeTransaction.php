6422320fae7e674a821f1898f22a3a2e31db82fe
sdaicz <sdaicz@klerede.com>
2016-12-23 13:40:25 -0300

    Adapt import siriusware transaction to new taxonomy. re #522

--- laravel/app/ImportQueryHandlers/SiriuswareBoxOfficeTransaction.php
+++ laravel/app/ImportQueryHandlers/SiriuswareBoxOfficeTransaction.php
@@ -1,12 +1,8 @@
 <?php
 namespace App\ImportQueryHandlers;
 
-use App\Channel;
 use App\Operator;
-use App\StatStatus;
-use App\VenueVariable;
-use Illuminate\Database\Query\JoinClause;
-use Illuminate\Support\Facades\DB;
+use DB;
 
 class SiriuswareBoxOfficeTransaction extends ImportQueryHandler {
 	protected $columns = ['source_id', 'register_id', 'sequence', 'time', 'operator_code'];
@@ -14,13 +10,16 @@ class SiriuswareBoxOfficeTransaction extends ImportQueryHandler {
 	protected $updateVarName = 'BOX_OFFICE_LAST_TRAN_ID';
 	function process() {
 		$this->addCodes('operator_code', Operator::class);
-		$cols1 = ['t.venue_id', 'source_id', 'register_id', 'sequence', 'time', 'o.id', 't.created_at', DB::raw('date(time)')];
-		$cols2 = ['venue_id', 'source_id', 'register_id', 'sequence', 'time', 'operator_id', 'created_at', 'business_day'];
+		$system_id = $this->query->import_query_class->system_id;
+		$cols1 = ['t.venue_id', DB::raw("$system_id as system_id"), 'source_id', 'register_id', 'sequence', 'time',
+			'o.id', 't.created_at', DB::raw('date(time)'), 'query_id'];
+		$cols2 = ['venue_id', 'system_id', 'source_id', 'register_id', 'sequence', 'time',
+			'operator_id', 'created_at', 'business_day', 'query_id'];
 		$sel = DB::table('import_siriusware_box_office_transaction as t')->where('query_id', $this->query->id)->select($cols1);
 		$sel->join('operator as o', 'operator_code', '=', 'o.code');
 		$sel->where('o.venue_id', $this->query->venue_id);
 		$colsString = implode(', ', $cols2);
-		$ins = "INSERT INTO box_office_transaction ($colsString) " . $sel->toSql();
+		$ins = "INSERT INTO transaction ($colsString) " . $sel->toSql();
 		DB::insert($ins, $sel->getBindings());
 		DB::table($this->getTableName())->where('query_id', $this->query->id)->update(['status'=>'imported']);
 	}
--- laravel/database/migrations/2016_12_20_154351_create_table_transaction.php
+++ laravel/database/migrations/2016_12_20_154351_create_table_transaction.php
@@ -15,7 +15,7 @@ class CreateTableTransaction extends Migration
         Schema::create('transaction', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('venue_id');
-            $table->integer('store_id');
+            $table->integer('store_id')->default(0);
             $table->integer('system_id');
             $table->integer('source_id');
             $table->integer('register_id');
@@ -23,7 +23,8 @@ class CreateTableTransaction extends Migration
             $table->date('business_day');
             $table->dateTime('time');
             $table->integer('operator_id');
-            $table->integer('batch_id');
+            $table->integer('batch_id')->nullable();
+            $table->integer('query_id')->nullable();
             $table->integer('member_xstore_id')->nullable();
             $table->timestamps();
         });
