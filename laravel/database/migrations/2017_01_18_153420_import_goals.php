45e2c1d9897a6b3489f1dcdb439be40f5dfa0b38
sdaicz <sdaicz@klerede.com>
2017-01-18 12:38:23 -0300

    Import partial sales goals for PSC. re #527

---
+++ laravel/database/migrations/2017_01_18_153420_import_goals.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\GoalsSales;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ImportGoals extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+		GoalsSales::import(1204, database_path('migrations/data/goals_psc.csv'));
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
