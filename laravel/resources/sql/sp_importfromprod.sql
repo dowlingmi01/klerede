9b747c5e607a563dcab2eebe09d3c712139aede4
sdaicz <sdaicz@klerede.com>
2017-01-25 12:43:01 -0300

    Adapt store sales stats import script to new taxonomy. re #523

--- laravel/resources/sql/sp_importfromprod.sql
+++ laravel/resources/sql/sp_importfromprod.sql
@@ -4,36 +4,40 @@ DELIMITER //
 
 CREATE PROCEDURE sp_importfromprod()
 BEGIN
+DECLARE store_category_id int
+;
 
+SELECT id INTO store_category_id FROM category WHERE code = 'store'
+;
 INSERT IGNORE INTO weather_daily
 SELECT * FROM klerede_prod.weather_daily
  WHERE venue_id in (1518, 1588, 1204, 1597, 1519, 1529, 1604)
 ;
 DELETE FROM stat_sales
  WHERE venue_id = 1588
-   AND channel_id = 4
+   AND category_id = store_category_id
    AND date > '2015-01-28'
 ;
 DELETE FROM stat_sales
  WHERE venue_id IN (1518, 1204, 1597, 1519, 1529, 1604)
-   AND channel_id = 4
+   AND category_id = store_category_id
 ;
 INSERT stat_sales
-     ( venue_id, date, year, quarter, month, week, channel_id, box_office_product_kind_id, membership_kind_id
-     , members, online, units, amount, created_at, updated_at
+     ( venue_id, date, year, quarter, month, week, category_id
+     , members, online, transactions, units, amount, created_at, updated_at
      )
-SELECT venue_id, date, year, quarter, month, week, channel_id, box_office_product_kind_id, membership_kind_id
-     , members, online, units, amount, created_at, updated_at
+SELECT venue_id, date, year, quarter, month, week, store_category_id
+     , members, online, units, units, amount, created_at, updated_at
   FROM klerede_prod.stat_sales
  WHERE venue_id = 1588
    AND date > '2015-01-28'
 ;
 INSERT stat_sales
-     ( venue_id, date, year, quarter, month, week, channel_id, box_office_product_kind_id, membership_kind_id
-     , members, online, units, amount, created_at, updated_at
+     ( venue_id, date, year, quarter, month, week, category_id
+     , members, online, transactions, units, amount, created_at, updated_at
      )
-SELECT venue_id, date, year, quarter, month, week, channel_id, box_office_product_kind_id, membership_kind_id
-     , members, online, units, amount, created_at, updated_at
+SELECT venue_id, date, year, quarter, month, week, store_category_id
+     , members, online, units, units, amount, created_at, updated_at
   FROM klerede_prod.stat_sales
  WHERE venue_id IN (1518, 1204, 1597, 1519, 1529, 1604)
 ;
