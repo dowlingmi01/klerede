9b11672b17a9f7e8bb1577eae03020a3dc29e0e8
sdaicz <sdaicz@klerede.com>
2017-01-30 13:54:16 -0300

    Add function to recategorize all products of a venue. re #522

--- laravel/app/Product.php
+++ laravel/app/Product.php
@@ -9,19 +9,29 @@ class Product extends Model {
 		return self::where(['venue_id'=>$venue_id, 'system_id'=>$system_id, 'code'=>$code])->first();
 	}
 	static public function import($data) {
-		$productMap = ProductCategoryMap::getFor($data->venue_id, $data->system_id, $data->mapping_code);
+		$product = self::firstOrNew(['venue_id'=>$data->venue_id, 'system_id'=>$data->system_id, 'code'=>$data->code]);
+		$product->fill((array) $data);
+		$product->setCategory();
+		$product->save();
+	}
+	public function setCategory() {
+		$productMap = ProductCategoryMap::getFor($this->venue_id, $this->system_id, $this->mapping_code);
 		$fields = ['category_id', 'is_unique_visitor', 'is_visitor', 'is_unit'];
 		if( $productMap ) {
 			foreach ($fields as $field) {
-				$data->$field = $productMap->$field;
+				$this->$field = $productMap->$field;
 			}
 		} else {
 			foreach ($fields as $field) {
-				$data->$field = 0;
+				$this->$field = 0;
 			}
 		}
-		$product = self::firstOrNew(['venue_id'=>$data->venue_id, 'system_id'=>$data->venue_id, 'code'=>$data->code]);
-		$product->fill((array) $data);
-		$product->save();
+	}
+	static public function reCategorizeAll($venue_id) {
+		$products = self::where('venue_id', $venue_id)->get();
+		foreach($products as $product) {
+			$product->setCategory();
+			$product->save();
+		}
 	}
 }
