4dcb89b15324117c439b34d4f408274d62a9b744
sdaicz <sdaicz@klerede.com>
2017-01-30 13:45:46 -0300

    Set up import init console command for new taxonomy. re #521

--- laravel/app/Console/Commands/ImportQueryInit.php
+++ laravel/app/Console/Commands/ImportQueryInit.php
@@ -2,6 +2,7 @@
 
 use App\ImportQuery;
 use App\ImportQueryClass;
+use App\VenueVariable;
 use Illuminate\Console\Command;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
@@ -44,9 +45,27 @@ class ImportQueryInit extends Command {
 //			1518 => [1100, 1200, 1300, 1400, 1500, 1600, 1700],
 //			1518 => [1800],
 //			1204 => [1100, 1200, 1300, 1400, 1500, 1600, 1700, 1900, 2000, 2100],
-			1518 => [2200],
-			1204 => [2200],
+//			1518 => [2200],
+//			1204 => [2200],
+			1518 => [1100, 1200, 1300, 1400, 1500, 1600, 1700, 2200],
+			1204 => [1100, 1200, 1300, 1400, 1500, 1600, 1700, 2200],
 		];
+
+		VenueVariable::setValue(1204, 'BOX_OFFICE_LAST_TRAN_ID', '0');
+		VenueVariable::setValue(1204, 'BOX_OFFICE_LAST_TRAN_DETAIL_ID', '0');
+		VenueVariable::setValue(1204, 'BOX_OFFICE_LAST_TRAN_MEMBER_INFO_ID', '0');
+		VenueVariable::setValue(1204, 'LAST_PASS_USAGE_ID', '0');
+		VenueVariable::setValue(1204, 'LAST_TICKET_USAGE_ID', '0');
+		VenueVariable::setValue(1204, 'MEMBERSHIP_LAST_UPDATE', '0');
+		VenueVariable::setValue(1518, 'BOX_OFFICE_LAST_TRAN_ID', '0');
+		VenueVariable::setValue(1518, 'BOX_OFFICE_LAST_TRAN_DETAIL_ID', '0');
+		VenueVariable::setValue(1518, 'BOX_OFFICE_LAST_TRAN_MEMBER_INFO_ID', '0');
+		VenueVariable::setValue(1518, 'LAST_PASS_USAGE_ID', '0');
+		VenueVariable::setValue(1518, 'LAST_TICKET_USAGE_ID', '0');
+		VenueVariable::setValue(1518, 'MEMBERSHIP_LAST_UPDATE', '0');
+
+		ImportQuery::where('status', 'pending')->update(['status'=>'executed']);
+
 		foreach($venues as $venue_id=>$iqcIds)
 			foreach ($iqcIds as $iqcId) {
 				/** @var ImportQueryClass $iqc */
