9e3fede414503bbcbbf60d1ecac9fd9263b34cb8
sdaicz <sdaicz@klerede.com>
2017-02-07 02:28:10 -0300

    Aqua categories and box definition changes. re #528

---
+++ laravel/database/migrations/2017_02_07_002613_aqua_category_changes.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Category;
+use App\Product;
+use App\ProductCategoryMap;
+use App\VenueCategory;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AquaCategoryChanges extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+		Category::import('categories.json');
+		VenueCategory::import(1518, 'venue_category_aqua.csv');
+		ProductCategoryMap::import(1518, 'pkm_aqua.csv');
+		Product::reCategorizeAll(1518);
+		$venue_data = App\Helpers\Helper::readCSV(database_path('migrations/data/boxes_aqua.csv'));
+		DB::table('dashboard_box_venue')->where('venue_id', 1518)->delete();
+		DB::table('dashboard_box_venue')->insert($venue_data);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
